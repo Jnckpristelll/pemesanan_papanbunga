62ac58247894561333dc001f9bf44c275708fc43
sailijar <sailijar88@gmail.com>
2025-06-19 23:39:10 +0700

    membuat migrate database dan laravel ui

---
+++ database/migrations/2025_06_19_151536_create_pesanans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_pelanggan');
+            $table->date('tgl_pesan');
+            $table->date('tgl_pengiriman');
+            $table->text('alamat_pengiriman');
+            $table->enum('status',['pending','berhasil','pengiriman']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanans');
+    }
+};
---
+++ database/migrations/2025_06_19_151549_create_detail_pesanans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_pesanans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_pesanan');
+            $table->bigInteger('id_produk');
+            $table->text('isi_papan');
+            $table->integer('jumlah');
+            $table->decimal('total_harga',12, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_pesanans');
+    }
+};
